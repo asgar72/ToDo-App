4310493c94f48b57798228568d5a9c390569b051
asgar72 <72asgar72@gmail.com>
2023-10-31 10:33:24 +0530

    create repository

---
+++ app/src/main/java/com/asgar72/todo/data/ToDoRepository.kt
@@ -0,0 +1,13 @@
+package com.asgar72.todo.data
+
+import androidx.lifecycle.LiveData
+
+class ToDoRepository (private val toDoDao: ToDoDao) {
+
+    val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+
+    suspend fun insertData(toDoData: ToDoData){
+        toDoDao.insertData(toDoData)
+    }
+
+}
